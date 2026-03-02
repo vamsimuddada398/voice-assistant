71d93b86e57281910e4257fa2a098ed7d590ffe6
Muddada Vamsi <vamsimuddada398@gmail.com>
2026-03-02 12:48:01 +0530

    first commit

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from langchain_huggingface import (
     HuggingFaceEndpoint,
     ChatHuggingFace,
 )
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
@@ -150,7 +151,7 @@ async def upload_pdf(files: List[UploadFile] = File(...)):
         with open(temp_path, "wb") as f:
             f.write(await file.read())
 
-        loader = PyPDFLoader(temp_path)
+        loader = PyMuPDFLoader(temp_path)
         documents = loader.load()
 
         splitter = RecursiveCharacterTextSplitter(
